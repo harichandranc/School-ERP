7be821eebb90f53efc04a2b206b3a3ca40be7a23
Harichandran C <harichstech@gmail.com>
2026-06-05 16:43:25 +0530

    Fix production API URL

--- erp-portal/client/src/api/axios.js
+++ erp-portal/client/src/api/axios.js
@@ -6,4 +6,21 @@ const API = axios.create({
   baseURL: import.meta.env.VITE_API_URL,
 });
 
+// Attach token automatically
+API.interceptors.request.use(
+  (config) => {
+    const user = JSON.parse(
+      localStorage.getItem("user")
+    );
+
+    if (user?.token) {
+      config.headers.Authorization =
+        `Bearer ${user.token}`;
+    }
+
+    return config;
+  },
+  (error) => Promise.reject(error)
+);
+
 export default API;
